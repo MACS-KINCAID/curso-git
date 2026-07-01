dbef51909e78babe20f13332a4fcc0e800a64985
MACS-KINCAID <mitsiu.carreno@upa.edu.mx>
2026-07-01 13:47:47 -0600

    update index

--- index.ts
+++ index.ts
@@ -2,3 +2,9 @@ router.route("/home");
 router.route("/settings");
 router.route("/config");
 router.route("/users");
+router.route("/printers");
+router.route("/hub");
+router.route("/institutes");
+router.route("/people");
+router.route("/articles");
+
